daaf1f84841a7257dcff6c99a9fe494f20eb51f7
Pinponsuhu Joseph <pinponsuhuomorinsola@gmail.com>
2022-09-03 15:55:46 +0100

    Begining form validation

--- lib/screens/authenticate/isLoggedIn.dart
+++ lib/screens/authenticate/isLoggedIn.dart
@@ -18,73 +18,85 @@ class _IsLoggedInState extends State<IsLoggedIn> {
       body: Center(
         child: SingleChildScrollView(
           child: SafeArea(
-            child: Column(
-              children: [
-                Image(
-                  height: 100,
-                  width: 100,
-                  image: AssetImage('assets/logo.png'),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Welcome Back",
-                        style: TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.indigo[900]),
-                      ),
-                      SizedBox(height: 20),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.indigo.shade100,
-                          borderRadius: BorderRadius.circular(4),
+            child: Form(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                children: [
+                  Image(
+                    height: 100,
+                    width: 100,
+                    image: AssetImage('assets/logo.png'),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Welcome Back",
+                          style: TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.indigo[900]),
                         ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: TextFormField(
-                            maxLength: 6,
-                            keyboardType: TextInputType.number,
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-                              enabledBorder: InputBorder.none,
-                              errorBorder: InputBorder.none,
-                              disabledBorder: InputBorder.none,
-                              hintText: "Pin",
+                        SizedBox(height: 20),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.indigo.shade100,
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: TextFormField(
+                                maxLength: 6,
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  focusedBorder: InputBorder.none,
+                                  enabledBorder: InputBorder.none,
+                                  disabledBorder: InputBorder.none,
+                                  hintText: "Pin",
+                                  errorStyle: TextStyle(
+                                    fontFamily: "Poppins",
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                validator: (value) {
+                                  if (value != null && value.length < 6) {
+                                    return 'Character less than 6';
+                                  } else {
+                                    return null;
+                                  }
+                                }),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 14,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/home');
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 19),
+                            child: Text(
+                              "Continue",
+                              style: TextStyle(fontSize: 18),
                             ),
                           ),
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  Colors.indigo[900])),
                         ),
-                      ),
-                      SizedBox(
-                        height: 14,
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/home');
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 40, vertical: 19),
-                          child: Text(
-                            "Continue",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        ),
-                        style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.indigo[900])),
-                      ),
-                    ],
-                  ),
-                )
-              ],
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
